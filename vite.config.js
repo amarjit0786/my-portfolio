fb437a277424b65424cb3cf2f7d5b9acc96e3e73
amarjit0786 <amrjitrai232@gmail.com>
2025-06-10 10:59:06 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  base: '/my-portfolio/', // 👈 GitHub repo name
+   plugins: [react(),tailwindcss()],
 });
